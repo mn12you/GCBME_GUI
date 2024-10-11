2137dcdc402208024b1b11263c7641a28f7875a3
mn12you <76604100+mn12you@users.noreply.github.com>
2024-10-11 17:07:05 +0800

    modified:   API.py 	new file:   API.spec 	modified:   GUI.ui 	modified:   GUI_ui.py 	new file:   Result/JOSA_result.txt 	new file:   Result/Test_group_result.txt 	new file:   Result/_result.txt 	new file:   Run_file/build/API/API.pkg 	new file:   Run_file/build/API/Analysis-00.toc 	new file:   Run_file/build/API/EXE-00.toc 	new file:   Run_file/build/API/PKG-00.toc 	new file:   Run_file/build/API/PYZ-00.pyz 	new file:   Run_file/build/API/PYZ-00.toc 	new file:   Run_file/build/API/base_library.zip 	new file:   Run_file/build/API/localpycs/pyimod01_archive.pyc 	new file:   Run_file/build/API/localpycs/pyimod02_importers.pyc 	new file:   Run_file/build/API/localpycs/pyimod03_ctypes.pyc 	new file:   Run_file/build/API/localpycs/pyimod04_pywin32.pyc 	new file:   Run_file/build/API/localpycs/struct.pyc 	new file:   Run_file/build/API/warn-API.txt 	new file:   Run_file/build/API/xref-API.html 	new file:   Run_file/dist/API.exe 	new file:   Run_file/dist/API.py 	new file:   Run_file/dist/API.spec 	new file:   Run_file/dist/GUI.ui 	new file:   Run_file/dist/GUI_ui.py 	new file:   Run_file/dist/Result/Test_group_result.txt 	new file:   Run_file/dist/SDK_aecg_100/AECG100.h 	new file:   Run_file/dist/SDK_aecg_100/AECG100x64.dll 	new file:   Run_file/dist/SDK_aecg_100/AECG100x64.lib 	new file:   Run_file/dist/SDK_aecg_100/AECG100x86.dll 	new file:   Run_file/dist/SDK_aecg_100/AECG100x86.lib 	new file:   Run_file/dist/SDK_aecg_100/API.chm 	new file:   Run_file/dist/SDK_aecg_100/libaecg.a 	new file:   Run_file/dist/SDK_aecg_100/libaecgrpix64.so 	new file:   Run_file/dist/SDK_aecg_100/libaecgrpix86.so 	new file:   Run_file/dist/SDK_aecg_100/libaecgx64.so 	new file:   Run_file/dist/SDK_aecg_100/libaecgx86.so 	new file:   Run_file/dist/SDK_mecg_20/API.chm 	new file:   Run_file/dist/SDK_mecg_20/MECG20.h 	new file:   Run_file/dist/SDK_mecg_20/MECG20x64.dll 	new file:   Run_file/dist/SDK_mecg_20/MECG20x86.dll 	new file:   Run_file/dist/SDK_mecg_20/libmecgrpix64.so 	new file:   Run_file/dist/SDK_mecg_20/libmecgrpix86.so 	new file:   Run_file/dist/SDK_mecg_20/libmecgx64.so 	new file:   Run_file/dist/SDK_mecg_20/libmecgx86.so 	new file:   Run_file/dist/SDK_secg_50/API.chm 	new file:   Run_file/dist/SDK_secg_50/SECG50.h 	new file:   Run_file/dist/SDK_secg_50/SECG50x64.dll 	new file:   Run_file/dist/SDK_secg_50/SECG50x64.lib 	new file:   Run_file/dist/SDK_secg_50/SECG50x86.dll 	new file:   Run_file/dist/SDK_secg_50/SECG50x86.lib 	new file:   Run_file/dist/aecg100.py 	new file:   Run_file/dist/mecg20.py 	new file:   Run_file/dist/secg50.py 	renamed:    requiorements.txt -> requirements.txt

--- API.py
+++ API.py
@@ -2,18 +2,23 @@ import sys
 import os
 import time
 from PyQt5 import uic
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets 
 from PyQt5.QtWidgets import QApplication, QMainWindow , QWidget, QVBoxLayout, QLabel, QPushButton, QComboBox,QFileDialog
-from PyQt5.QtCore import  QThread, pyqtSignal, QObject
+from PyQt5.QtCore import  QThread, pyqtSignal, QObject, QMutex, QWaitCondition
+from sklearn.metrics import accuracy_score, confusion_matrix
 import serial.tools.list_ports
 from ctypes import *
 from mecg20 import *
 from aecg100 import *
 from secg50 import *
+# Transform for model result
 Label_reader={'Normal':0,'AFib':1}
-
+Label_transform={0:'Normal',1:'AFib'}
+# AECG100 control parameter
 ac = None
 dc = None
+
+# AECG100 load txt function
 def load_raw_data_file(file_name):
     global ac, dc
     
@@ -40,11 +45,10 @@ def load_raw_data_file(file_name):
     play_raw_data.OutputSignalCallback =CFUNCTYPE(None, c_double, c_int, c_int)(0)
     return play_raw_data
 
+# woker for sending ECG 
 class Worker(QObject):
     finished = pyqtSignal()
-    label=pyqtSignal(int)
-    result = pyqtSignal(str)
-    AI_pred=pyqtSignal(int)
+    result = pyqtSignal(int,int,float)
     
     def __init__(self,device,data_path,ser,device_name):
         super().__init__()
@@ -52,6 +56,10 @@ class Worker(QObject):
         self.data_path=data_path
         self.ser=ser
         self.device_name=device_name
+        self._is_paused = False
+        self._is_running = True
+        self._mutex = QMutex()
+        self._pause_condition = QWaitCondition()
     
     def run(self):
         # Simulate a long-running task
@@ -69,13 +77,23 @@ class Worker(QObject):
             data_folder=self.data_path
 
         for dirpath, dirnames,files_in_directory in os.walk(data_folder):
-            QApplication.processEvents() 
+            
+            self._mutex.lock()
+            if self._is_paused:
+                self._pause_condition.wait(self._mutex)
+            self._mutex.unlock()
+            
+            if not self._is_running:
+                self.device.free()
+                break
+
             for file in files_in_directory:
                 file_path=os.path.join(dirpath,file)
-                if dirpath.find("Afib"):
-                    self.label.emit(0)
+                print(file_path)
+                if  file_path.find("Afib")!=-1:
+                    label_encode=1
                 else:
-                    self.label.emit(1)
+                    label_encode=0
                 send_str="k"
                 self.ser.write(send_str.encode())
 
@@ -116,19 +134,40 @@ class Worker(QObject):
                         self.device.free()
                         sys.exit()             
                     self.device.output_waveform(0, None, None)   # Send ECG to simulator 
+                start_time=time.time()
                 while 1:
                  
                     if self.ser.inWaiting() > 0:
                         result = self.ser.readline()
                         decoded_data = result.decode('ascii').strip()     # Get OKEY and start to send ECG
-                        self.AI_pred.emit(Label_reader[decoded_data[1:]])
-                        self.result.emit(decoded_data[1:])
+                        end_time=time.time()
+                        self.result.emit(Label_reader[decoded_data[1:]],label_encode,(end_time-start_time))
                         if self.device_name==0:
                             self.device.stop_output()
                         elif self.device_name==1:
                             self.device.set_output_function(OutputFunction.Output_Off.value)
                         break
+        self.device.free()
+        self.finished.emit()
 
+    def pause(self):
+        self._mutex.lock()
+        self._is_paused = True
+        self._mutex.unlock()
+
+    def resume(self):
+        self._mutex.lock()
+        self._is_paused = False
+        self._pause_condition.wakeAll()
+        self._mutex.unlock()
+
+    def stop(self):
+        self._is_running = False
+        if self._is_paused:
+            self.resume()  # In case it's paused, resume and stop
+
+
+# Combobox class to add up click update function
 class ComboBox(QtWidgets.QComboBox):
     popupAboutToBeShown = QtCore.pyqtSignal()
 
@@ -136,25 +175,29 @@ class ComboBox(QtWidgets.QComboBox):
         self.popupAboutToBeShown.emit()
         super(ComboBox, self).showPopup()
 
+# the main window class for the GUI
 class GCBME_app(QMainWindow):
+    
+    #initial function
     def __init__(self):
         super().__init__()
         uic.loadUi('GUI.ui',self)
+        
+        self.device_name=0 #device flag AECG100:'0' ; SECG50:'1'
+        self.ser = serial.Serial()# serial port
+        self.AI_pred=[]# AI result
+        self.label=[]#label result
+        self.latency=[]#latency result
+        self.pause_button_state=0 # button state PAUSE:'0' ; RESUME:'1'
+        self.start_button_state=0 # button state START:'0' ; STOP:'1'
 
-        # self.device = MECG20()
-        self.device_name=0
-        self.ser = serial.Serial()
-        self.button_pause=True
-        self.AI_pred=[]
-        self.label=[]
-
+        # UI initiate 
         self.Serial_buttn.clicked.connect(self.connect_port)
         self.Serial_item.popupAboutToBeShown.connect(self.update_ports)
         self.data_buttn.clicked.connect(self.choose_dataset)        
-        self.Pause_buttn.clicked.connect(self.pause_it)
         self.Start_buttn.clicked.connect(self.start_multi)
-
-
+        self.pause_resume.clicked.connect(self.pause_resume_task)
+        self.Export_result.clicked.connect(self.export)
 
     def update_ports(self):
         """Updates the list of available serial ports."""
@@ -162,10 +205,9 @@ class GCBME_app(QMainWindow):
         ports = serial.tools.list_ports.comports()
         for port in ports:
             self.Serial_item.addItem(port.device)  # Add port name to combo box
-    
-    def pause_it(self):
-        self.button_pause=False
+
     def connect_port(self):
+        """Create Serial connection according to selected port"""
         comport=self.Serial_item.currentText()
         if comport!="":
             baudrate=115200
@@ -196,9 +238,9 @@ class GCBME_app(QMainWindow):
             print("Comport not select!")
             
     def choose_dataset(self):
+        '''Dataset select through QFileDialog'''
         # Open the folder dialog and allow the user to select a folder
         folder_path = QFileDialog.getExistingDirectory(self, "Select Folder")
-
         # If the user selects a folder, update the label with the folder path
         if folder_path:
             self.data_path.setText(folder_path)
@@ -206,46 +248,107 @@ class GCBME_app(QMainWindow):
             self.data_path.setText("No folder selected")
         
     def start_multi(self):
-        self.AI_pred=[]
-        self.label=[]
-        if self.ECG_device_box.currentText()=="AECG100":
-            self.device=AECG100()
-            self.device_name=0
+        '''Start send ECG work'''
+        if self.start_button_state==0:
+            self.Terminal_result.clear()
+            self.Final_result.clear()
+            self.start_button_state=1
+            self.Start_buttn.setText("Stop")
+
+            self.AI_pred=[]
+            self.label=[]
+            self.latency=[]
+
+            if self.ECG_device_box.currentText()=="AECG100":
+                self.device=AECG100()
+                self.device_name=0
+            else:
+                self.device=SECG50()
+                self.device_name=1
+
+            # Create a QThread object
+            self.thread = QThread()
+            # Create a worker object
+            self.worker = Worker(self.device,self.data_path.text(),self.ser,self.device_name)
+            # Move the worker to the thread
+            self.worker.moveToThread(self.thread)
+            # Connect the thread's started signal to the worker's run method
+            self.thread.started.connect(self.worker.run)
+            # Connect the worker's finished signal to stop the thread
+            self.worker.finished.connect(self.thread.quit)
+            # Optionally, delete the worker and thread when done
+            self.worker.finished.connect(self.worker.deleteLater)
+            self.thread.finished.connect(self.thread.deleteLater)
+            self.worker.finished.connect(self.final_report)
+            # Connect worker's progress signal to a handler method
+            self.worker.result.connect(self.report_result)
+            # Start the thread
+            self.thread.start()
         else:
-            self.device=SECG50()
-            self.device_name=1
+            self.start_button_state=0
+            self.Start_buttn.setText("Start")
+            self.worker.stop()
 
-        # Create a QThread object
-        self.thread = QThread()
-        # Create a worker object
-        self.worker = Worker(self.device,self.data_path.text(),self.ser,self.device_name)
-        # Move the worker to the thread
-        self.worker.moveToThread(self.thread)
-        # Connect the thread's started signal to the worker's run method
-        self.thread.started.connect(self.worker.run)
-        # Connect the worker's finished signal to stop the thread
-        self.worker.finished.connect(self.thread.quit)
-        # Optionally, delete the worker and thread when done
-        self.worker.finished.connect(self.worker.deleteLater)
-        self.thread.finished.connect(self.thread.deleteLater)
-        # Connect worker's progress signal to a handler method
-        self.worker.result.connect(self.report_result)
-        self.worker.AI_pred.connect(self.AI_result)
-        self.worker.label.connect(self.label_result)
-        # Start the thread
-        self.thread.start()
 
-    def report_result(self,decoded_data):
-        self.Terminal_result.append(decoded_data)
-    def AI_result(self,result):
-        self.AI_pred.append(result)
-        print(f'AI pred:{self.AI_pred}')
-    def label_result(self,result):
-        self.label.append(result)
+
+
+    def report_result(self,AI,label,latency):
+        '''Receive label result signal and Save it in array'''
+        latency=round(latency,4)
+        self.label.append(label)
+        self.AI_pred.append(AI)
+        self.latency.append(latency)
+        acc=round(100*accuracy_score(self.label, self.AI_pred),3)
         print(f'Label:{self.label}')
+        print(f'AI pred:{self.AI_pred}')
+        print(f'Latency :{self.latency}')
+        result_sentece=f"################\nAI prediction: {Label_transform[AI]}\nLabel: {Label_transform[label]}\nLatency: {latency}\nAccuracy so far: {acc}\n################"
+        self.Terminal_result.append(result_sentece)
+    
+    def final_report(self):
+        acc=round(100*accuracy_score(self.label, self.AI_pred),3)
+        cm=confusion_matrix(self.label, self.AI_pred)
+        latency=round(sum(self.latency) / len(self.latency),3)
+        col_width = 30
+        confusion_matrix_str=""
+        confusion_matrix_str += f"{'':<{col_width}}{'Predicted Normal':<{col_width}}{'Predicted Afib':<{col_width}}\n"
+        confusion_matrix_str += f"{'Actual Normal':<{col_width}}{cm[0][0]:<{col_width}}{cm[0][1]:<{col_width}}\n"
+        confusion_matrix_str += f"{'Actual Afib':<{col_width}}{cm[1][0]:<{col_width}}{cm[1][1]:<{col_width}}\n"
+        result_sentence=f"##########\n The accuracy is: {acc}\n The latency is: {latency}\nConfusion matrix is:\n"
+        self.Final_result.append(result_sentence)
+        self.Final_result.append(confusion_matrix_str)
+        print(confusion_matrix_str)
+
+    def pause_resume_task(self):
+        if self.pause_button_state==0:
+            self.pause_button_state=1
+            self.pause_resume.setText("Resume")
+            self.worker.pause()
+        else:
+            self.pause_button_state=0
+            self.pause_resume.setText("Pause")
+            self.worker.resume()
+    
+    def export(self):
+        string_to_save=self.Final_result.toPlainText()
+        group=self.Group_name.toPlainText()
+        with open("./Result/"+group+"_result.txt", "w") as text_file:
+            text_file.write(string_to_save)
+        
         
 
-#     def start_test(self):
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = GCBME_app()
+    window.show()
+    app.exec_()
+
+
+
+##### old code for one thread #####
+# def start_test(self):
 #         self.button_pause=True
 #         self.AI_pred=[]
 #         self.label=[]
@@ -334,9 +437,3 @@ class GCBME_app(QMainWindow):
 #                         elif self.device_name==1:
 #                             self.device.set_output_function(OutputFunction.Output_Off.value)
 #                         break    
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = GCBME_app()
-    window.show()
-    sys.exit(app.exec_())
